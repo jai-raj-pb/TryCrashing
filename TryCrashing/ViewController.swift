535353af06a4e958edf879ce32194c6454dd1614
Jai Raj Srivastava <jai.srivastava@mindtickle.com>
2022-12-20 00:56:55 +0530

    section crash

--- TryCrashing/ViewController.swift
+++ TryCrashing/ViewController.swift
@@ -10,38 +10,50 @@ import UIKit
 class ViewController: UIViewController {
     @Atomic private var numbers: Set<Date> = Set<Date>()
     
+    @IBAction func b(_ sender: Any) {
+        print("pp")
+        let st = Set<Int>([1])
+        let stt = Set<Int>([0])
+        n -= 1
+        tableView.deleteSections(IndexSet(st), with: .automatic)
+        tableView.reloadSections(IndexSet(stt), with: .automatic)
+    }
+    
+    @IBOutlet var tableView: UITableView!
+
+    var data: [Int] = []
+    var n: Int = 2
     var mediaTimer = Timer()
 
     override func viewDidLoad() {
+        tableView.dataSource = self
         super.viewDidLoad()
         _numbers.mutate {
             $0.insert(Date())
         }
-        ff()
-    }
-    
-    func ff(){
-        for i in 0...1000{
-            self.f1(i: i)
-        }
-    }
-    
-    func check(_ num: Date) {
-        if !numbers.contains(num) {
-            print("contains \(num)")
-            _numbers.mutate{
-                $0.insert(Date())
-            }
-        }
-    }
-
-    func f1(i: Int) {
-        let t1 = Thread {
-            print("from f1:\(Thread.current.name!) Count:\(self.numbers.count)")
-            self.check(Date())
-        }
-        t1.name = "Thread \(i)"
-        t1.start()
+        
+        let st2 = Set<Int>([0, 1])
+        tableView.insertSections(IndexSet(st2), with: .automatic)
+        tableView.reloadData()
+            
+        
     }
 }
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        n
+    }
+}
+
+
+
 
